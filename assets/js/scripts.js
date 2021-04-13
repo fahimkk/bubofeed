245683d7cf5e5d55da9bd48b404bd65c1973dc0e
fahimkk <fahimasharafkk@gmail.com>
2021-04-13 12:49:28 +0530

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,102 @@
+
+$(".menu-btn").click(function(){
+    $(".menu-btn").hide();
+    $("#menu-btn-close").show();
+    $(".wrapper").show();
+    $('html, body').css({
+        overflow: 'hidden',
+        height: '100%'
+    });
+});
+
+$("#menu-btn-close").click(function(){
+    $("#menu-btn-close").hide();
+    $(".menu-btn").show();
+    $(".wrapper").hide();
+    $('html, body').css({
+        overflow: 'auto',
+        height: 'auto'
+    });
+});
+
+$(".wrapper a").click(function(){
+    $("#menu-btn-close").hide();
+    $(".menu-btn").show();
+    $(".wrapper").hide()
+    $('html, body').css({
+        overflow: 'auto',
+        height: 'auto'
+    })
+});
+
+// Home Section 
+$('#home-carousel').owlCarousel({
+    autoplay: true,
+    autoplayHoverPause: true,
+    loop: true,
+    dots: true,
+    items:1,
+    responsive:{
+    // display resolution
+        0:{
+            items: 1, 
+        },
+        485: {
+            items: 1,
+        },
+        728: {
+            items: 1,
+        },
+        960: {
+            items: 1,
+        },
+        1200: {
+            items: 1,
+        }
+    }
+});
+
+// Gallery section 
+$('#gallery-carousel').owlCarousel({
+    autoplay: true,
+    autoplayHoverPause: true,
+    loop: true,
+    items:1,
+    nav: true,
+    dots: true,
+    responsive:{
+    // display resolution
+        0:{
+            items: 1, 
+            dots: false,
+        },
+        485: {
+            items: 1,
+        },
+        728: {
+            items: 1,
+        },
+        960: {
+            items: 1,
+        },
+        1200: {
+            items: 1,
+        }
+    }
+});
+
+$( window ).on( "load", function() {
+    mainNav();
+    $(window).scroll(function() {
+        mainNav();
+    });
+    function mainNav() {
+        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        if (top){
+        $('.navbar').start().animate({
+            "opacity": "1",
+            "top": "0",
+        });
+        }
+	}
+     });
